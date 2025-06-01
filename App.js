0a1080b5b3782433dde13a0b1eb6d42ebcc5a798
Gustavo Ferreira <gustavofrsnt@gmail.com>
2025-05-31 21:53:40 -0300

    Switching to mobile engine

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    color: "white"
+  }
+});
